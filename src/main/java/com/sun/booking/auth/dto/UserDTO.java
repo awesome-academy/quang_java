bac513dfcaeb15a6bfb348a6b4c12018d8a9c480
quangnd-3189 <nguyen.duc.quang@sun-asterisk.com>
2026-03-24 08:06:28 +0700

    Feature #97916 : Đăng ký tài khoản & Security Config (Form Login)
    https://edu-redmine.sun-asterisk.vn/issues/97916

---
+++ src/main/java/com/sun/booking/auth/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package com.sun.booking.auth.dto;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+  private Long id;
+  private String username;
+  private String email;
+  private List<String> roles;
+}
--- src/main/java/com/sun/booking/users/UserRepository.java
+++ src/main/java/com/sun/booking/users/UserRepository.java
@@ -15,4 +15,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
+
+    Optional<User> findBySocialId(String socialId);
+
+    boolean existsBySocialId(String socialId);
 }
